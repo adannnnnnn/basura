08db6888f41fab5e2960caf216b9dc50a88820f3
adannnnnnn <125261009+adannnnnnn@users.noreply.github.com>
2023-02-13 10:51:44 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,5 @@
+basic.show_string("HOLA")
+
+def on_forever():
+    basic.show_string("MUNDO")
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . # . # .
+    . . # . .
+    # # # # #
+    `)
+basic.pause(2000)
+basic.clearScreen()
+basic.forever(function () {
+	
+})
